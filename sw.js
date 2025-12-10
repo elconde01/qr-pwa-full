492d2589c6f91b334ffc62ac8037876bc3769fbe
elconde01 <43607388+elconde01@users.noreply.github.com>
2025-12-10 12:15:36 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'pwa-qr-v1';
+const ASSETS = [ '/', '/index.html', '/manifest.json', 'https://unpkg.com/html5-qrcode@2.3.8/minified/html5-qrcode.min.js' ];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e=>{ self.clients.claim(); });
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+});
